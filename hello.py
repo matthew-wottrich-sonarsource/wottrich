6febf7d5500409d1eae20478763455f912b037fb
matthew-wottrich-sonarsource <matthew.wottrich@sonarsource.com>
2023-04-28 09:07:45 -0500

    1

--- hello.py
+++ hello.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 print('helloworlds')
 print('helloworlds')
 print('helloworlds')
@@ -16,16 +15,11 @@ print('helloworlds')
 print('helloworlds')
 print('helloworlds')
 print('helloworlds')
-<<<<<<< HEAD
-=======
 print('helloworlds')
 print('helloworlds')
 print('helloworlds')
 print('helloworlds')
-=======
 print('helloworld')
 print('test')
 print('testing')
 print('broken')
->>>>>>> 2a921973d0ae26deb58453378b00cd531c7e1b15
->>>>>>> 487620c8ca3d5fc28665bb1660dd44fd37e60594
