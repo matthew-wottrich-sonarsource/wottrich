8cd34e5d34e2376b64c38fb5daf3b92507e13836
matthew-wottrich-sonarsource <107486144+matthew-wottrich-sonarsource@users.noreply.github.com>
2024-04-16 15:04:45 -0500

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,6 +9,7 @@ for fizzbuzz in range(100):
         print("buzz")
         continue
     print(fizzbuzz)
+    print(anewedit)
 
 #for fizzbuzz in range(100):
 #    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
