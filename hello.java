cc88fdf34c7e8eb3c06ccc9418d422336e65f1a4
matthew-wottrich-sonarsource <107486144+matthew-wottrich-sonarsource@users.noreply.github.com>
2024-05-24 11:33:24 -0500

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,12 @@
+/* HelloWorld.java
+ */
+
+import javax.validation.constraints.NotNull;
+
+public class HelloWorld
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello again World!");
+	}
+}
