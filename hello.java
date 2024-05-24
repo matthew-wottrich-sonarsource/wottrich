3ad03f61cc5b7b342ad2f67213ab1880a12dd719
matthew-wottrich-sonarsource <107486144+matthew-wottrich-sonarsource@users.noreply.github.com>
2024-05-24 11:43:49 -0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -7,6 +7,13 @@ const jwt = require('jsonwebtoken');
 
 jwt.sign(payload, key, { algorithm: 'none' }); // Noncompliant
 
+const jwt = require('jsonwebtoken');
+
+jwt.verify(token, key, {
+    expiresIn: 360000,
+    algorithms: ['none'] // Noncompliant
+}, callbackcheck);
+
 public class HelloWorld
 {
 	public static void main(String[] args) {
