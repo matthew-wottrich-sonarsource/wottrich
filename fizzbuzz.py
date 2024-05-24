7e8f6bcc4dc153f01a28050d50a74e2d2ffd05c3
matthew-wottrich-sonarsource <107486144+matthew-wottrich-sonarsource@users.noreply.github.com>
2024-05-24 09:43:56 -0500

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -33,3 +33,27 @@ for fizzbuzz in range(100):
 #        print("buzz")
 #        continue
 #    print(fizzbuzz)
+
+def func_x(num):
+    if num == 1:
+        return a()
+    elif num == 2:
+        return b()
+    elif num == 3:
+        return c()
+    elif num == 4:
+        return d()
+    elif num == 5:
+        return e()
+
+
+# Better
+def func_y(num):
+    mapper = {
+        1: a,
+        2: b,
+        3: c,
+        4: d,
+        5: e
+    }
+    return mapper[num]()
